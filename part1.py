3b89ca8e01e249e543b92420bef3e28ca1e22b92
Haofan Yang <haofanyoung@gmail.com>
2020-04-27 20:07:55 -0400

    added reduced ppmi

--- part1.py
+++ part1.py
@@ -1,5 +1,6 @@
 import numpy as np
 from collections import defaultdict
+import scipy.linalg as scipy_linalg
 
 class word_vectors():
     def __init__(self):
@@ -57,7 +58,18 @@ class word_vectors():
         f = lambda x: 0.0 if x <= 1 else np.log(x) 
         f = np.vectorize(f)
         self.ppmi = f(self.ppmi)
-    
+
+        # Build reduced PPMI
+        U, E, Vt = scipy_linalg.svd(self.ppmi, full_matrices=False)
+        U = np.matrix(U) # compute U
+        E = np.matrix(np.diag(E)) # compute E
+        Vt = np.matrix(Vt) # compute Vt = conjugage transpose of V
+        V = Vt.T # compute V = conjugate transpose of Vt
+        UE = np.matrix(np.matmul(U, E))
+        UEVt = np.matrix(np.matmul(UE, Vt))
+        self.__verify_SVD_reconstruction(UEVt)
+        self.reduced_ppmi = self.ppmi * V[:, 0:3]
+
     def get_word_vec_ppmi(self, word):
         word_index = self.word_dict[word]
         return self.ppmi[word_index].copy()
@@ -65,13 +77,29 @@ class word_vectors():
     def get_word_vec_raw(self, word):
         word_index = self.word_dict[word]
         return self.raw_count[word_index].copy()
+
+    def get_word_vec_reduced_ppmi(self, word):
+        word_index = self.word_dict[word]
+        return self.reduced_ppmi[word_index].copy()
     
     def compute_distance(self, word1, word2):
         vec1 = self.get_word_vec_ppmi(word1)
         vec2 = self.get_word_vec_ppmi(word2)
         diff = vec1 - vec2
         return np.linalg.norm(diff)
-
+    
+    # Verify that matrices multiplications reproduce the original PPMI matrix
+    # Print error messages if failed to do so
+    def __verify_SVD_reconstruction(self, UEVt):
+        diff = self.ppmi - UEVt
+        filter_small = lambda x : False if abs(x) < 1e-14 else True
+        filter_small = np.vectorize(filter_small)
+        booleans = filter_small(diff)
+        large_elements = np.extract(booleans, diff)
+        if len(large_elements) != 0:
+            print("===============Failed to reconstruct PPMI matrix from SVD===============")
+            print("===============DIFF (PPMI - UEVt)===============")
+            print(diff)
 
 if __name__ == '__main__':
     wvs = word_vectors()
